f99aa788b977a767c48e8368473b4d26b4cbd29c
FieryInferno <bagassetia271@gmail.com>
2025-07-27 02:51:01 +0700

    feat: show error

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,11 +1,11 @@
+'use server'
+
 import Validator from '@/app/validator'
 
 const validator = new Validator()
 const LoginFormSchema = validator.object({ email: validator.string().email() })
 
 export const login = async (state: void | { errors?: { email?: string[] } } | undefined, formData: FormData) => {
-  'use server'
-
   const validatedFields = LoginFormSchema.safeParse({ email: formData.get('email') })
 
   if (!validatedFields.success) return { errors: validatedFields.error.fieldErrors }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,17 @@ import { useActionState } from 'react';
 
 export default function Home() {
   const [state, action, pending] = useActionState(login, undefined)
+
   return (
     <div className="login-container">
       <form className="login-form" action={action}>
         <h2>Login</h2>
         <label htmlFor="email">Email</label>
         <input type="email" id="email" placeholder="you@example.com" required />
+        {state?.errors.email?.map((error) => <li className='text-red-600 text-[.8rem]'>{error}</li>)}
         <label htmlFor="password">Password</label>
         <input type="password" id="password" placeholder="••••••••" required />
-        <button type="submit">Login</button>
+        <button type="submit">{pending ? 'Loading' : 'Login'}</button>
       </form>
     </div>
   );
