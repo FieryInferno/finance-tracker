15f0ec9bc78e6ca478361747544000a09f661108
FieryInferno <bagassetia271@gmail.com>
2025-08-12 17:48:18 +0700

    button delete category

--- src/app/(admin)/categories/@listCategories/category-item.component.tsx
+++ src/app/(admin)/categories/@listCategories/category-item.component.tsx
@@ -1,11 +1,12 @@
 import styles from './category-item.module.css'
+import ButtonDeleteCategory from './components/button-delete-category.component'
 
 export default function CategoryItem({ color, name }: { color: string, name: string }) {
   return (
     <li className={styles['kategori-item']}>
       <span className={styles['kategori-warna']} style={{backgroundColor: color}}></span>
       <span className={styles['kategori-nama']}>{name}</span>
-      <button className={styles['kategori-delete']}>Delete</button>
+      <ButtonDeleteCategory />
     </li>
   )
 }
---
+++ src/app/(admin)/categories/@listCategories/components/button-delete-category.component.tsx
@@ -0,0 +1,3 @@
+export default function ButtonDeleteCategory() {
+  return <button className="bg-[#ef4444] hover:bg-[#dc2626] px-3 py-1.5 border-none rounded-md text-white cursor-pointer">Delete</button>
+}
