949babfcb2f7d8e4e4dd606ae85358a4bfede63d
FieryInferno <bagassetia271@gmail.com>
2025-08-18 22:46:51 +0700

    feat(ui): confirmation before delete
    - install sweetalert2

--- src/app/(admin)/categories/list-categories.component/components/button-delete-category.component.tsx
+++ src/app/(admin)/categories/list-categories.component/components/button-delete-category.component.tsx
@@ -1,12 +1,25 @@
 'use client'
 
+import Swal from 'sweetalert2'
+
 // import { deleteCategory } from '../../_actions/actions'
 
 export default function ButtonDeleteCategory() {
   const onclick = async () => {
-    const confirmed = confirm('Are you sure?')
+    const result = await Swal.fire({
+      title: 'Are you sure!',
+      text: 'Do you want to delete this category?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    })
 
-    // if (confirmed) console.log(await deleteCategory())
+    if (result.isConfirmed) {
+      // User clicked "Yes!", proceed with deletion
+      // console.log(await deleteCategory())
+    }
   }
 
   return <button className="bg-[#ef4444] hover:bg-[#dc2626] px-3 py-1.5 border-none rounded-md text-white cursor-pointer" onClick={onclick}>Delete</button>
